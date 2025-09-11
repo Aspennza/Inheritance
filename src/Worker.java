ae9db1a4b43a1df95dff299037353f4dc9222562
Zoe Aspenns <aspennza@mail.uc.edu>
2025-09-11 13:15:21 -0400

    calculateWeeklyPay function created

--- src/Worker.java
+++ src/Worker.java
@@ -25,4 +25,27 @@ public class Worker extends Person
     public void setHourlyPayRate(double hourlyPayRate) {
         this.hourlyPayRate = hourlyPayRate;
     }
+
+    public double calculateWeeklyPay(double hoursWorked)
+    {
+        double weeklyPay = 0.00;
+        double payRegularRate = 0.00;
+        double payOvertime = 0.00;
+        int hoursAtRegularRate = 0;
+        double hoursOvertime = 0;
+
+            if (hoursWorked <= 40)
+            {
+                weeklyPay = hoursWorked * hourlyPayRate;
+            } else
+            {
+                hoursAtRegularRate = 40;
+                hoursOvertime = hoursWorked - hoursAtRegularRate;
+
+                payRegularRate = hoursAtRegularRate * hourlyPayRate;
+                payOvertime = hoursOvertime * (hourlyPayRate * 1.5);
+                weeklyPay = payRegularRate + payOvertime;
+            }
+        return weeklyPay;
+    }
 }
