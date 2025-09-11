6f535d0c7701a5e4d7f30e403b434f844d3a6b44
Zoe Aspenns <aspennza@mail.uc.edu>
2025-09-11 16:06:30 -0400

    Created three SalaryWorker objects in InheritanceDemo and added all instances to the workers ArrayList

--- src/InheritanceDemo.java
+++ src/InheritanceDemo.java
@@ -1,4 +1,5 @@
 //Need to:
+//finish main class
 //create WorkerTest.java & SalaryWorkerTest.java
 //check if you need to override equals
 //check if there's a way to have a second constructor for salaryworker
@@ -14,5 +15,18 @@ public class InheritanceDemo
         Worker john = new Worker("000001", "John", "Doe", "Mr.", 1980, 25.00);
         Worker jane = new Worker("000002", "Jane", "Doe", "Ms.", 1979, 26.00);
         Worker sally = new Worker("000003", "Sally", "Williams", "Mrs.", 1985, 23.00);
+
+        SalaryWorker bella = new SalaryWorker("000004", "Bella", "Goth", "Mrs.", 2000, 40.00, 83200.00);
+        SalaryWorker johnny = new SalaryWorker("000005", "Johnny", "Zest", "Mr.", 2003, 37.00, 76960.00);
+        SalaryWorker nancy = new SalaryWorker("000006", "Nancy", "Landgraab", "Mrs.", 2000, 41.00, 85280.00);
+
+        workers.add(john);
+        workers.add(jane);
+        workers.add(sally);
+        workers.add(bella);
+        workers.add(johnny);
+        workers.add(nancy);
+
+        //create the weekly pay periods loop here
     }
 }
