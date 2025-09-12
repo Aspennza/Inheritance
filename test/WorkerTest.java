3d00c6a890e69887e6b48fbf20a76d2c663ef993
Zoe Aspenns <aspennza@mail.uc.edu>
2025-09-12 16:53:10 -0400

    completed testWorkerConstructor1 and 2

--- test/WorkerTest.java
+++ test/WorkerTest.java
@@ -15,7 +15,33 @@ class WorkerTest {
     }
 
     @Test
+    void testWorkerConstructor1()
+    {
+        Worker worker3 = new Worker("000003", "Sally", "Williams", "Mrs.", 1979, 30.00);
+        assertEquals("000003", worker3.getID());
+        assertEquals("Sally", worker3.getFirstName());
+        assertEquals("Williams", worker3.getLastName());
+        assertEquals("Mrs.", worker3.getTitle());
+        assertEquals(1979, worker3.getYOB());
+        assertEquals(30.00, worker3.getHourlyPayRate());
+    }
+
+    @Test
+    void testWorkerConstructor2()
+    {
+        Person person1 = new Person("000001", "Sally", "Williams", "Mrs.", 1979);
+        Worker worker3 = new Worker(person1, 30.00);
+        assertEquals("000001", worker3.getID());
+        assertEquals("Sally", worker3.getFirstName());
+        assertEquals("Williams", worker3.getLastName());
+        assertEquals("Mrs.", worker3.getTitle());
+        assertEquals(1979, worker3.getYOB());
+        assertEquals(30.00, worker3.getHourlyPayRate());
+    }
+
+    @Test
     void setFirstName() {
+
     }
 
     @Test
